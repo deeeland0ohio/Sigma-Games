943490bf00660d0403c9d1370e50c416c224040b
Dylan Russo <Dylandukerusso@gmail.com>
2026-03-22 22:59:51 -0700

    refactor: Remove unused fetch test files
    These files were used for testing CDN fetches and are no longer needed for the project's functionality.

--- src/fetch-test4.js
+++
@@ -1,7 +0,0 @@
-import https from 'https';
-
-const url = 'https://cdn.jsdelivr.net/gh/genizy/web-port@main/ultrakill/Build/ultrakill.loader.js';
-
-https.request(url, {method: 'HEAD'}, (res) => {
-  console.log(`${url}: ${res.statusCode} - ${res.headers['content-length']} bytes`);
-}).end();
