81f79d84ce263b3ee4bdd53fac08abbceb92dee8
Dylan Russo <Dylandukerusso@gmail.com>
2026-03-27 13:32:16 -0700

    feat: Add Baseball Bros and Football Bros games
    This commit introduces two new games, Baseball Bros and Football Bros, to the portal. It includes their respective HTML files for rendering and TypeScript definitions for game data. Additionally, metadata such as developer, tags, rating, and plays has been removed from existing game definitions to streamline the data structure.

---
+++ src/data/games/football-bros.ts
@@ -0,0 +1,13 @@
+import { Game } from "../../types";
+import { Trophy } from "lucide-react";
+
+export const footballBros: Game = {
+  id: "football-bros",
+  title: "Football Bros",
+  description:
+    "Online multiplayer football! Bone crushing hits, long bombs, and tons more!",
+  icon: Trophy,
+  type: "iframe",
+  url: "/games/football-bros/index.html",
+  popularity: 85,
+};
