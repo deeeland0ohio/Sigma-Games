c1fc0794f35ccdd9ff08f278e2af20bb5fa4765e
Dylan Russo <Dylandukerusso@gmail.com>
2026-03-28 19:06:18 -0700

    feat: Remove Bowmasters game integration
    The Bowmasters game and its associated files have been removed from the application. This includes its entry in the game data index, the HTML file for the game, and its TypeScript definition.

--- src/data/games/index.ts
+++ src/data/games/index.ts
@@ -64,7 +64,6 @@ import { idleBreakout } from "./idle-breakout";
 import { basketRandom } from "./basket-random";
 import { blockBlast } from "./block-blast";
 import { blackJack } from "./black-jack";
-import { bowMasters } from "./bow-masters";
 import { tombOfTheMask } from "./tomb-of-the-mask";
 import { soccerRandom } from "./soccer-random";
 import { granny2 } from "./granny-2";
@@ -166,7 +165,6 @@ export const allGames: Game[] = [
   basketRandom,
   blockBlast,
   blackJack,
-  bowMasters,
   tombOfTheMask,
   soccerRandom,
   granny2,
